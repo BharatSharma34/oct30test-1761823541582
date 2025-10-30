54d21bfe5d88f094b25424ec4a6303445897cf82
BharatSharma34 <bharatsharma@ducktaleit.com>
2025-10-30 16:55:43 +0530

    Create tool.py

---
+++ tool.py
@@ -0,0 +1,27 @@
+"""
+oct30Test - Custom Lambda Tool
+Description: test
+
+IMPORTANT: Only edit the code in the main() function below.
+The Lambda handler will be automatically appended during deployment.
+DO NOT add lambda_handler code here - it will be added automatically.
+"""
+
+def main():
+    """
+    Main function for oct30Test
+    This function will be called by the Lambda handler.
+    
+    
+    Returns:
+    dict - JSON-serializable response
+    """
+    # Your tool logic here
+    
+    return {
+        "success": True,
+        "message": "Hello from oct30Test!",
+        "data": {}
+    }
+
+# You can add helper functions below
